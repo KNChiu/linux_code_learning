301cf43a34a80d9a7d456142cdbfce8c324a94cd
KNChiu <y9760210@gmail.com>
2021-12-03 10:26:16 +0800

    完整答案包含驗證輸出

--- c/1_Two_Sum.c
+++ c/1_Two_Sum.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cstdlib>
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     int i=0,j=0;
     int *ans=(int*)malloc(2*sizeof(int));
@@ -18,3 +19,25 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     *returnSize=2;
     return ans;
     }
+
+
+int main(int argc, char *argv[ ]){
+    int nums[] = {2, 7, 11, 15};
+
+    int numsSize = sizeof(nums)/sizeof(int);
+    int target = 9;
+    int returnSize = 0;
+    int *return_array = NULL;
+    
+    return_array = twoSum(nums, numsSize, target, &returnSize);
+
+
+    if (return_array == NULL)
+        return 0;
+
+    for (int i = 0; i < returnSize; i++){
+        printf("%d ", return_array[i]);
+    }
+
+    return 0;
+}
