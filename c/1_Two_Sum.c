ea6b4bb68da78d0f7934933e6bd6168b93021b4d
KNChiu <y9760210@gmail.com>
2021-12-09 17:07:02 +0800

    修改程式碼

--- c/1_Two_Sum.c
+++ c/1_Two_Sum.c
@@ -2,21 +2,21 @@
 #include <cstdlib>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    int i=0,j=0;
-    int *ans=(int*)malloc(2*sizeof(int));
-    for(i=0;i<numsSize-1;i++)
+    int i = 0,j = 0;
+    int* ans = (int*)malloc(2*sizeof(int));
+    for(i = 0;i < numsSize - 1;i++)
     {
-        for(j=i+1;j<numsSize;j++)
+        for(j = i + 1;j < numsSize;j++)
         {
-            if(nums[i]+nums[j]==target)
+            if(nums[i] + nums[j] == target)
             {
-                ans[0]=i;
-                ans[1]=j;
+                ans[0] = i;
+                ans[1] = j;
                 break;
             }
         }
     }
-    *returnSize=2;
+    *returnSize = 2;
     return ans;
     }
 
@@ -25,7 +25,7 @@ int main(int argc, char *argv[ ]){
     int nums[] = {2, 7, 11, 15};
 
     int numsSize = sizeof(nums)/sizeof(int);
-    int target = 9;
+    int target = 22;
     int returnSize = 0;
     int *return_array = NULL;
     
