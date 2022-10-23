cc0688f4dccae746c147a57495cf6fe37163b5e5
KNChiu <y9760210@gmail.com>
2022-10-23 11:47:42 +0800

    leetcode 20. Valid Parentheses

--- cpp/20.cpp
+++ cpp/20.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
 class Solution {
-public:
+public:    
     bool isValid(string s) {
-
+        stack<char> st;
+        
+        for (char c: s) {            
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else if (st.empty()) {
+                return false;
+            }            
+            else if (st.top() == '(' && c != ')') return false;
+            else if (st.top() == '{' && c != '}') return false;
+            else if (st.top() == '[' && c != ']') return false;
+            
+            // Pop
+            else st.pop();
+        }
+        
+        return (st.empty());
     }
 };
 
+int main() {
+    string s = "{[]";
+    Solution sol;
+    bool ans = sol.isValid(s);
 
-int main(void){
+    out << ans << " ";
+    
+    return 0;
+}
 
