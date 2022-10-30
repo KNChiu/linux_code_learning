8bc33f79cdc91167cb1437ef327eb8a8e3fb392a
KNChiu <y9760210@gmail.com>
2022-10-30 13:43:29 +0800

    98. Validate Binary Search Tree

---
+++ cpp/98.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (root == NULL) return true;
+        
+        TreeNode* node;
+
+        if (root->left!=NULL){
+            node = root->left;
+            while (node->right != NULL)
+                node = node->right;
+            if (node->val >= root->val)
+                return false;
+            }
+        
+
+        if (root->right != NULL){
+            node = root->right;
+            while (node->left != NULL)
+                node = node->left;
+            if (node->val <= root->val)
+                return false;
+        }
+        
+        return isValidBST(root->left) && isValidBST(root->right);
+    }   
+};
